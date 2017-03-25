2e8fa7d10cc4b9758ee6ceb61d127b50b84c5d97
Asa Jones <ajones@oneclearchoice.com>
2017-03-25 02:09:25 -0400

    Scaffold Application

---
+++ src/app/default-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import './default-view.component.html';
+import './default-view.component.scss';
+
+@Component({
+  selector: 'default-view',
+  templateUrl: './default-view.component.html',
+  styleUrls: [ './default-view.component.scss' ]
+})
+export class DefaultViewComponent {
+
+}
