80219415bfe1e778e5a006d46bf14a12b3e6aadb
Asa Jones <ajones@oneclearchoice.com>
2017-03-27 01:36:13 -0400

    Create dataservice using the in-memory-data-service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 
@@ -11,15 +10,19 @@ import { DefaultViewComponent } from './default-view.component';
 import { HeartButtonComponent } from './heart-button.component';
 import { LizardMessageComponent } from './lizard-message.component';
 
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+import { MessageService } from './message.service';
+
 import './rxjs-extensions';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 0 }),
   ],
   declarations: [
     AppComponent,
@@ -27,7 +30,7 @@ import './rxjs-extensions';
     HeartButtonComponent,
     LizardMessageComponent
   ],
-  providers: [],
+  providers: [ MessageService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- src/app/heart-button.component.ts
+++ src/app/heart-button.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import './heart-button.component.html';
 import './heart-button.component.scss';
 
+import { MessageService } from './message.service';
+
 @Component({
   selector: 'heart-button',
   templateUrl: './heart-button.component.html',
   styleUrls: [ './heart-button.component.scss' ]
 })
 export class HeartButtonComponent {
-
+  constructor(private messageService: MessageService) {}
+  replaceMessage(): void {
+    this.messageService.next();
+  }
 }
